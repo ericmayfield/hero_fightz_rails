cee8eaaa50021da43acc1ad041ef506b1c58b345
Eric Mayfield <eric@visiblycreative.com>
2019-04-02 09:54:53 -0400

    Set up /logout route to sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
     def create
+        #add if/else struct, check for auth, using binding
+        #for omniauth generate securerand u.password = method_from securerand
         @user = User.find_by(email: params[:email])
         return head(:forbidden) unless @user.authenticate(params[:password])
         session[:user_id] = @user.id
