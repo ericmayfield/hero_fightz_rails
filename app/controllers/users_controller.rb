b728f6d8a3edfd2efa861a13027cee2282fae20e
Eric Mayfield <eric@visiblycreative.com>
2019-04-04 20:14:39 -0400

    Added Update/Edit Actions & updated destroy logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
     before_action :load_user, only: [:show, :edit, :update, :destroy]
+    before_action :is_user, except: [:index, :new, :create]
 
     def new
-        @user = User.new
+        # Checks if a user is logged in before allowing account creation
+        if !logged_in?
+            @user = User.new
+        else
+            redirect_to account_path
+        end
     end
 
     def create
@@ -11,18 +17,25 @@ class UsersController < ApplicationController
     end
 
     def show
-        is_user
     end
 
     def edit
-        is_user
     end
 
     def update
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to account_path
+        else
+            redirect_to account_edit_path
+        end
     end
 
     def destroy
         @user.delete
+        session.delete :user_id
+        redirect_to root_path
     end
      
     private
@@ -36,6 +49,14 @@ class UsersController < ApplicationController
             @user = User.find(session[:user_id])
         else
             redirect_to login_path
+        end
+    end
+
+    #Checks if current user logged in matches user record trying to view
+    def is_user
+        if params[:id] && params[:id] != current_user.id
+            flash[:alert] = "You do not have access to the content you were attempting to view."
+            redirect_to account_path
         end
     end
 end
