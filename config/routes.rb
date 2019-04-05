a303974a5fbe34f0b1afdf45ec75c1b218a12f47
Eric Mayfield <eric@visiblycreative.com>
2019-04-04 20:03:55 -0400

    Added static account/edit route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do 
+    resources :heros, only: [:new]
+  end
+
   resources :heros
 
-  resources :teams do #do index to view heroes or individual hero in a team, /teams/1/heros/1 check if current user is on team otherwise redirect
+  resources :teams do
     resources :heros, only: [:index, :show]
   end
 
@@ -12,6 +15,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   get '/logout' => 'sessions#destroy'
   get '/account' => 'users#show'
+  get 'account/edit' => 'users#edit'
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
