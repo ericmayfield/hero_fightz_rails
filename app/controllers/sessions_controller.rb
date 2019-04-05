ae7d2354e8cbccd344119465799ea05a6d93c066
Eric Mayfield <eric@visiblycreative.com>
2019-04-05 08:52:30 -0400

    Fixed redirect path to account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(email: params[:email])
         if @user.try(:authenticate, params[:password])
             session[:user_id] = @user.id
-            redirect_to user_path(@user.id)
+            redirect_to account_path
         else
             flash[:alert] = "Username or Password Incorrect"
             redirect_to login_path
