f7bdff6c75896e656007f336561231df0244c261
Eric Mayfield <eric@visiblycreative.com>
2019-04-05 09:32:43 -0400

    Removed unncessary helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in?, :is_user, :belongs_to_user
-
-    # def belongs_to_user?(record)
-        
-    # end
+    helper_method :current_user, :logged_in?
 
     # Returns the current logged-in user (if any).
     def current_user
@@ -13,16 +9,5 @@ class ApplicationController < ActionController::Base
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
         !current_user.nil?
-    end
-
-    #Checks if current user logged in matches user record trying to view
-    def is_user
-        if params[:id]
-            puts params[:id]
-            if params[:id] != current_user.id
-                flash[:alert] = "You don't have access to view this content."
-                redirect_to account_path(current_user)
-            end
-        end
     end
 end
